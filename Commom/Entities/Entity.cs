3e0a173bbf37e5bb1d13b3708d9ea1bb9e3d8cb9
DanielMendesdoAmaral <daniel.amaral720@gmail.com>
2021-05-05 20:22:47 -0300

    Entity e Order

---
+++ Commom/Entities/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Commom.Entities
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.Now;
+        }
+    }
+}
---
+++ Domains/Entities/Order.cs
@@ -0,0 +1,41 @@
+﻿using Commom.Entities;
+using Commom.Enum;
+using System;
+using System.Collections.Generic;
+
+namespace Domains.Entities
+{
+    public class Order : Entity
+    {
+        public string Description { get; private set; }
+        public EnOrderType Type { get; private set; }
+        public string NFE { get; private set; }
+        public string ToCNPJ { get; private set; }
+        public DateTime? FinishedAt { get; private set; }
+        public int AttemptsNumber { get; private set; }
+        public List<string> UrlsEvidencesImages { get; private set; }
+        public Local Local { get; private set; }
+        public Company Company { get; private set; }
+
+        public Order(
+            string description,
+            EnOrderType type,
+            string nfe,
+            string toCNPJ,
+            int attemptsNumber,
+            Local local,
+            Company company
+        )
+        {
+            Description = description;
+            Type = type;
+            NFE = nfe;
+            ToCNPJ = toCNPJ;
+            FinishedAt = null;
+            AttemptsNumber = 0;
+            UrlsEvidencesImages = new List<string>();
+            Local = local;
+            Company = company;
+        }
+    }
+}
