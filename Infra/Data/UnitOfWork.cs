d8872bcef2bb816b07a53189a10b96d21f9ae495
DanielMendesdoAmaral <daniel.amaral720@gmail.com>
2021-05-06 12:19:12 -0300

    Unit of Work

---
+++ Infra/Data/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Infra.Data.Contexts;
+
+namespace Infra.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext _context;
+
+        public UnitOfWork(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
