aa28c541691a6584f97d69687bcda92d42a94eec
kaiquethedog <52429335+kaiquethedog@users.noreply.github.com>
2021-05-10 17:14:25 -0300

    Criada algumas das interfaces

---
+++ Domains/Repositories/ICompany.cs
@@ -0,0 +1,19 @@
+﻿using Domains.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Domains.Repositories
+{
+    public interface ICompany
+    {
+        List<Company> List(Guid idCompany, string name = null);
+        Company Search(Guid id);
+        Company Search(string cnpj);
+        Company Add(Company company);
+        Company Change(Company company);
+        void Remove(Guid id);
+        void AddShipper(ShipperCompany shipperCompany);
+        void ChangeShipper(ShipperCompany shipperCompany);
+        void DeleteShipper(Guid id);
+    }
+}
---
+++ Domains/Repositories/IOrder.cs
@@ -0,0 +1,16 @@
+﻿using Domains.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Domains.Repositories
+{
+    public interface IOrder
+    {
+        List<Order> List(Guid idOrder, string name = null);
+        Order Search(Guid id);
+        Order Search(string cnpj);
+        Order Add(Order order);
+        Order Change(Order order);
+        void Remove(Guid id);
+    }
+}
