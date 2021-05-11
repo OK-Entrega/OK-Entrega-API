cbb4af4a19e2559922441b2867576976f8ec52bf
kaiquethedog <52429335+kaiquethedog@users.noreply.github.com>
2021-05-11 16:26:01 -0300

    Adicionado Commands e Queries referentes ao embarcador

---
+++ Domains/Queries/Shipper/ListShipperPerId.cs
@@ -0,0 +1,12 @@
+﻿using Commom.Queries;
+using System;
+
+namespace Domains.Queries.Shipper
+{
+    public class ListShipperPerId : IQuery
+    {
+        public Guid ShipperId { get; set; }
+        public void Validate()
+        { }
+    }
+}
