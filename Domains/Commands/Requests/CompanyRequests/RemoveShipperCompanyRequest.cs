03c93001590c133555467c297cc2e64356b43702
kaiquethedog <52429335+kaiquethedog@users.noreply.github.com>
2021-05-12 15:30:48 -0300

    Command e Queries referentes a company

---
+++ Domains/Commands/Requests/CompanyRequests/RemoveShipperCompanyRequest.cs
@@ -0,0 +1,12 @@
+﻿using Commom.Commands;
+using System;
+
+namespace Domains.Commands.Company
+{
+    public class RemoveShipperCompanyCommand : ICommand
+    {
+        public Guid CompanyId { get; set; }
+        public Guid ShipperCompanyId { get; set; }
+        public void Validate() {}
+    }
+}
---
+++ Domains/Queries/Request/CompanyRequest/ListCompanyDetailsRequest.cs
@@ -0,0 +1,25 @@
+﻿using Commom.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domains.Queries.Company
+{
+    public class ListCompanyDetailsQuery : IQuery
+    {
+        public Guid CompanyId { get; set; }
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string CNPJ { get; set; }
+        public void Validate(){}
+        public ListCompanyDetailsQuery(string name = null)
+        {
+            if (name != null)
+            {
+                Name = name.Trim().ToLower();
+            }
+        }
+    }
+}
